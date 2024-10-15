81475120c805106ba4ed8d335a083158af2382ac
Dana <chertilas@mail.ru>
2024-10-15 13:38:36 +0300

    Realized test_bun.py Passed

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,5 @@ from Diplom_1.bun import Bun
 # Фикстура для создания объекта Bun
 @pytest.fixture
 def bun_fixture():
-    return Bun(name="Classic Bun", price=1.99)
+    return Bun(name= "Classic Bun", price=199)
+
