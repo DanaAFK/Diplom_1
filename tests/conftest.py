167a38fafe2c1cf2ff9c8c01a5d4c608540d1ca8
Dana <chertilas@mail.ru>
2024-10-16 13:20:04 +0300

    Realized test_ingredient passed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from Diplom_1.bun import Bun
 from Diplom_1.ingredient import Ingredient
+from Diplom_1.burger import Burger
 
-# Фикстура для создания объекта Bun
 @pytest.fixture
 def bun_fixture():
     return Bun(name="Classic Bun", price=199)
@@ -11,3 +11,9 @@ def bun_fixture():
 def ingredient_fixture():
     return Ingredient(ingredient_type="Начинки", name="Биокотлета", price=300)
 
+@pytest.fixture
+def burger_fixture(bun_fixture, ingredient_fixture):
+    burger = Burger()
+    burger.set_buns(bun_fixture)
+    burger.add_ingredient(ingredient_fixture)
+    return burger
